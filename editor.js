e4e7ef1b2a9386599d2fa41145146554880484b6
RosieRose07 <128731183+RosieRose07@users.noreply.github.com>
2023-06-04 16:57:59 +0000

    Deleted save button listener

--- editor.js
+++ editor.js
@@ -666,7 +666,6 @@ document.getElementById("secondaryOpen").addEventListener("click", function() { 
 document.getElementById("defaultOpen").click();
 
 // save, load, and run buttons
-document.getElementById("saveButton").addEventListener("click", loadTableToDiagram);
 document.getElementById("loadButton").addEventListener("click", function() { load(); });
 
 document.getElementById("load-actual-button").addEventListener("change", loadModel);
